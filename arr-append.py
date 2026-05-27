94a9cd7161a6e1ee6a4c36a608f78419118e816a
Senaaravichandran <senaaravichandran@gmail.com>
2026-05-27 10:18:57 +0530

    arr-append

--- arr-append.py
+++ arr-append.py
@@ -1,6 +1,6 @@
 arr=[]
-elements=int(input("Enter the no of elements:"))
+elements = int(input("Enter the no of elements:"))
 for x in range(elements):
     data = int(input("enter the elements: "))
     arr.append(data)
-    print(data)
+    print(arr)
