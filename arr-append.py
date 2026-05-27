94d8eefa037921ad65104d8eb942b5c4e87f0017
Senaaravichandran <senaaravichandran@gmail.com>
2026-05-27 10:45:45 +0530

    only even

--- arr-append.py
+++ arr-append.py
@@ -2,5 +2,6 @@ arr=[]
 elements = int(input("Enter the no of elements:"))
 for x in range(elements):
     data = int(input("enter the elements: "))
-    arr.append(data)
-    print(arr)
+    if data % 2 == 0:
+        arr.append(data)    
+        print(arr)
