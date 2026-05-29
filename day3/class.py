fbfc20a2f1c847beb6621482ce5f5ba0c857a9b7
Senaaravichandran <senaaravichandran@gmail.com>
2026-05-29 14:21:50 +0530

    class learned perfectly and the method was perfect

---
+++ day3/class.py
@@ -0,0 +1,14 @@
+class it:
+    def student(self):
+        print("Senaa")
+class aids:
+    def student(self):
+        print("Venky")
+class cse:
+    def student(self):
+        print("Jerry")
+smvec = [it(), aids(), cse()]
+for clg in smvec:
+    clg.student()
+print(len("smvec"))
+print(max(1,2,3))
---
+++ day3/own-digit.py
@@ -0,0 +1,9 @@
+class add:
+    def __init__(self, arr1, arr2):
+        self.arr01=arr1
+        self.arr02=arr2
+    def aadi(self):
+        print(self.arr01+self.arr02)
+x=[1,2,3]
+y=[4,5,6]
+cars=add(x,y)
