55f8cf343ddfaedfc5ac333aca0b590a7c04c21b
Senaaravichandran <senaaravichandran@gmail.com>
2026-05-29 10:03:43 +0530

    day 3 training

---
+++ day3/class-method.py
@@ -0,0 +1,10 @@
+class car:
+    def __init__(self, arr1, arr2):
+        self.arr01=arr1
+        self.arr02=arr2
+    def aadi(self):
+        print(self.arr01+self.arr02)
+x=[1,2,3]
+y=[4,5,6]
+cars=car(x,y)
+cars.aadi()
